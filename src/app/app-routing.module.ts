950cc8848d2f765b96ea58d35c810719ad89df40
jchocha <jchocha@asite.com>
2023-12-13 14:46:21 +0530

    Add a catch-all route to redirect to the dashboard. (generated by blackbox.ai )

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'dashboard' , pathMatch: 'full'},
   { path: 'multiple-iframe', loadChildren: () => import('./multiple-iframe/multiple-iframe.module').then(m => m.MultipleIframeModule), data: { id: 'navmultipleiframe' } },
   { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule), data: { id: 'navdashboard' } },
+  { path: '**', redirectTo: 'dashboard' , pathMatch: 'full' }
 ];
 
 @NgModule({
